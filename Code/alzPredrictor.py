f525644b2f17d752a9b493224b861f260dc4af86
Francisco Carrillo Pérez <carrilloperezfrancisco@gmail.com>
2017-06-09 14:06:39 +0200

    Corrected erros, formatting some text and correctly shutting down the program when close buttom is pressed

--- Code/alzPredrictor.py
+++ Code/alzPredrictor.py
@@ -11,6 +11,7 @@
 #----------------------------------------------------------------------
 
 from sys import argv
+from sys import exit
 from sys import platform as _platform
 import subprocess
 import os
@@ -32,7 +33,7 @@ class MainWindow():
         self.prediction = ""
         screen_width = main.winfo_screenwidth()
         screen_height = main.winfo_screenheight()
-
+        self.filename_name_only = filename.split("/")
         #Fullscreen
         main.attributes("-fullscreen", True)
 
@@ -46,7 +47,8 @@ class MainWindow():
         """ You can change the place of the images giving
         different values to the screen_width and screen_height """
         self.image_on_canvas = self.canvas.create_image((screen_width/2)-100, (screen_height/2)-200, anchor = NW, image = self.image)
-        self.texto = self.canvas.create_text((screen_width)-(screen_width/2),50,font=("Purisa", 16),text = self.filename)
+        self.texto = self.canvas.create_text((screen_width)-(screen_width/2),50,font=("Purisa", 16),
+        text = self.filename_name_only[len(self.filename_name_only)-1])
         self.prediction_text = self.canvas.create_text((screen_width)-(screen_width/2),(screen_height/2)-300,font=("Purisa", 16),
         text = "Predicted label is: "+self.prediction)
 
@@ -60,7 +62,7 @@ class MainWindow():
         """ You can change the place of the button giving
         different values to the screen_width and screen_height """
         #self.button.grid(row=4, column=10,columnspan=2, rowspan=2)
-        self.button_nextimage.place(x=(screen_width/2)-150,y=0)
+        self.button_nextimage.place(x=(screen_width/2)-120,y=0)
 
         # button to change to previous image
         self.button_previous = Button(main, text="Predict", command=self.predict(filename))
@@ -81,12 +83,15 @@ class MainWindow():
         new_filename = fd.askopenfilename(parent=self.main) # show an "Open" dialog box and return the path to the selected file
         self.image = ImageTk.PhotoImage(PIL.Image.open(new_filename))
         # change image
-        self.canvas.itemconfigure(self.texto, text=new_filename)
+        self.filename_name_only = new_filename.split("/")
+        self.canvas.itemconfigure(self.texto,
+        text=self.filename_name_only[len(self.filename_name_only)-1])
         self.canvas.itemconfig(self.image_on_canvas, image = self.image)
 
     def closeButton(self):
         #Close button
         self.main.destroy()
+        sys.exit()
 
     def toggle_fullscreen(self, main):
         self.state = not self.state  # Just toggling the boolean
