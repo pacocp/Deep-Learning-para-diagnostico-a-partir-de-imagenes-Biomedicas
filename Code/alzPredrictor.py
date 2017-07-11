265cf9af82416f1f37c94192e4de92d410060fb3
Francisco Carrillo Pérez <carrilloperezfrancisco@gmail.com>
2017-07-11 18:54:39 +0200

    Changes in the main script and in the software for prediction

--- Code/alzPredrictor.py
+++ Code/alzPredrictor.py
@@ -36,25 +36,25 @@ class MainWindow():
 		self.filename = filename
 		self.prediction = ""
 		self.df_results = df_results
-		screen_width = main.winfo_screenwidth()
-		screen_height = main.winfo_screenheight()
+		self.screen_width = main.winfo_screenwidth()
+		self.screen_height = main.winfo_screenheight()
 		self.filename_name_only = filename.split("/")
 		#Fullscreen
 		#main.attributes("-fullscreen", True)
 
 		# canvas for image
-		self.canvas = Canvas(main, width=screen_width, height=screen_height)
-		self.canvas.grid(row=screen_height, column=screen_width)
+		self.canvas = Canvas(main, width=self.screen_width, height=self.screen_height)
+		self.canvas.grid(row=self.screen_height, column=self.screen_width)
 		self.state = False
 
 		self.image = ImageTk.PhotoImage(PIL.Image.open(self.filename))
 		# set first image on canvas
 		""" You can change the place of the images giving
-		different values to the screen_width and screen_height """
-		self.image_on_canvas = self.canvas.create_image((screen_width/2)-100, (screen_height/2)-200, anchor = NW, image = self.image)
-		self.texto = self.canvas.create_text((screen_width)-(screen_width/2),50,font=("Purisa", 16),
+		different values to the self.screen_width and self.screen_height """
+		self.image_on_canvas = self.canvas.create_image((self.screen_width/2)-100, (self.screen_height/2)-200, anchor = NW, image = self.image)
+		self.texto = self.canvas.create_text((self.screen_width)-(self.screen_width/2),50,font=("Purisa", 16),
 		text = self.filename_name_only[len(self.filename_name_only)-1])
-		self.prediction_text = self.canvas.create_text((screen_width)-(screen_width/2),(screen_height/2)-300,font=("Purisa", 16),
+		self.prediction_text = self.canvas.create_text((self.screen_width)-(self.screen_width/2),(self.screen_height/2)-300,font=("Purisa", 16),
 		text = "Predicted label is: "+self.prediction)
 
 		 # button to close
@@ -65,31 +65,34 @@ class MainWindow():
 		# button to change to next image
 		self.button_chooseImage = Button(main, text="Choose Image", command=self.chooseImage)
 		""" You can change the place of the button giving
-		different values to the screen_width and screen_height """
+		different values to the self.screen_width and self.screen_height """
 		#self.button.grid(row=4, column=10,columnspan=2, rowspan=2)
-		self.button_chooseImage.place(x=(screen_width/2)-120,y=0)
+		self.button_chooseImage.place(x=(self.screen_width/2)-120,y=0)
 
 		# button to change to predict image
-		self.button_predict = Button(main, text="Predict", command=self.predict(filename))
+		self.button_predict = Button(main, text="Predict", command=self.predict)
 		#self.button.grid(row=4, column=11)
 		""" You can change the place of the button giving
-		different values to the screen_width and screen_height """
-		self.button_predict.place(x=(screen_width/2),y=0)
+		different values to the self.screen_width and self.screen_height """
+		self.button_predict.place(x=(self.screen_width/2),y=0)
 
 
 	#----------------
 	# BUTTONS
 	#----------------
-	def predict(self,filename):
+	def predict(self):
 		'''Predicting the label of the image with keras model'''
-		prediction = predict_class(filename)
-		data_of_training = {'ID': len(self.df_results.index),'NAME': filename, 'PREDICTED LABEL': prediction[0]}
+		self.prediction = predict_class(self.filename)
+		data_of_training = {'ID': len(self.df_results.index),'NAME': filename, 'PREDICTED LABEL': self.prediction}
 		df_aux = pd.DataFrame(data=[data_of_training])
 		self.df_results = self.df_results.append(df_aux, ignore_index=True)
+		self.canvas.itemconfigure(self.prediction_text,
+		text="Predicted label is: "+str(self.prediction))
 	def chooseImage(self):
 		'''Choose the image for predicting'''
 		Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 		new_filename = fd.askopenfilename(parent=self.main) # show an "Open" dialog box and return the path to the selected file
+		self.filename = new_filename
 		self.image = ImageTk.PhotoImage(PIL.Image.open(new_filename))
 		# change image
 		self.filename_name_only = new_filename.split("/")
